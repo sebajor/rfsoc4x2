8402ef022655606f04a3df2f2aad9cb94ed2c110
seba <seba@localhost.localdomain>
2023-11-26 16:27:36 -0300

    fix some issues with the ps code

--- axil_test/os_code/axil_test.py
+++ axil_test/os_code/axil_test.py
@@ -4,21 +4,22 @@ import os
 import struct
 import numpy as np
 
-filename = '~/bitfiles/fpga.bin'
+filename = '/home/casper/bitfiles/axil_test/pga.bin'
 
 dev_mem_file = '/dev/mem'
 
 reg_info = {
-        'baseaddr'  :0xA110_0000,
+        'baseaddr'  :0xA1100000,
         'size'      :8*8
         }
 bram_info = {
-        'baseaddr'  :0xA000_0000,
+        'baseaddr'  :0xA0000000,
         'size'      :2**10*4*8
         }
 
 ##program the pl using the pcap
 os.system('sudo fpgautil -b %s -f Force'%filename)
+os.system('sudo chmod 777 %s' %dev_mem_file)
 
 fpga_mem = open(dev_mem_file, 'r+b')
 
@@ -30,13 +31,13 @@ bram = mmap.mmap(fpga_mem, bram_info['size'], offset=bram_info['baseaddr'])
 reg[8:12] = struct.pack('I', 1)
 data = np.random.randint(2**32-1, size=2**10).astype(np.int32)
 for i in range(len(data)):
-    reg[:4] = struct.pack('I', data[i])
     reg[4:8] = struct.pack('I', i)
+    reg[:4] = struct.pack('I', data[i])
 reg[8:12] = struct.pack('I', 0)
 
 ##now read back the data from the bram
 for i in range(len(data)):
-    bram_dat = struct.unpack('I', bram[4*i:4*(i+1)])
+    bram_dat = struct.unpack('I', bram[4*i:4*(i+1)])[0]
     print("addr:%i gold:%i bram:%i"%(i,data[i], bram_dat))
     assert(data[i]==bram_dat)
 
